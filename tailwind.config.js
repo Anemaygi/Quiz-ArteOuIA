ed7d90a86ec29d45dfc686cf0802e8464388318a
Anemaygi <gianemayumisan@gmail.com>
2023-10-25 23:31:46 -0300

    style: add style colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        persianRed: '#CD2F3B',
+        wine: '#702127',
+        night: '#121212',
+        timberWolf: '#D7CDCC',
+        grayTheme: '#282828',
+      }
+    },
   },
   plugins: [],
 }
