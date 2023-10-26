f8ee65389c901cd7a8e47198d9c389e440392407
Anemaygi <gianemayumisan@gmail.com>
2023-10-25 23:44:20 -0300

    feat: button component

---
+++ src/components/button.tsx
@@ -0,0 +1,22 @@
+import { FiArrowRight } from "react-icons/fi";
+import React from 'react';
+
+
+interface ButtonProps {
+  handleClick: () => void;
+  text: string;
+}
+
+const Button: React.FC<ButtonProps> = ({handleClick, text}) => {
+
+  return (
+  <div className="border rounded-full border-solid border-grayTheme m-2 cursor-pointer hover:scale-105 transition-all" onClick={handleClick}>
+    <div className="text-white border rounded-full border-solid border-timberWolf p-2 m-1.5 flex">
+          <p className="flex flex-grow px-3 py-0.5 text-lg">{text}</p>
+          <p className="flex items-center"><FiArrowRight size={24}/></p>
+    </div>
+  </div>
+  );
+};
+
+export default Button;
