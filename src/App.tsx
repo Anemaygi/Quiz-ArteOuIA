1706ed2a0805e0b975aa75a89e13c325114b134c
Anemaygi <gianemayumisan@gmail.com>
2023-10-25 23:03:33 -0300

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import RootRouter from './routes'
+import './App.css'
+
+function App() {
+
+  return (
+    <RootRouter />
+  )
+}
+
+export default App
---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+
+  return (
+  <h1 className="text-3xl font-bold underline">
+      Trabalho de SMC - Página inicial
+    </h1>
+  );
+};
+
+export default LandingPage;
