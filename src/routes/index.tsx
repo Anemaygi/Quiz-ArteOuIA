6cc6bcbbf8a0d77737b2ea4ad12594cf891e437f
Anemaygi <gianemayumisan@gmail.com>
2023-10-28 14:54:43 -0300

    feat: add quiz result

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import LandingPage from "../pages/landingPage";
 import Quiz from "../pages/quiz_onboarding";
 import QuizPlay from "../pages/quiz_play";
+import QuizResult from "../pages/quiz_result";
 
 export default function RootRouter() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/quiz' element={<Quiz />} />
-      <Route path='/quizplay' element={<QuizPlay />} />s
+      <Route path='/quizplay' element={<QuizPlay />} />
+      <Route path='/quizresult' element={<QuizResult />} />
     </Routes>
   );
 }
