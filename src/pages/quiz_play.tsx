53cf41795f915ac3e5cf4f6d70b952eeb43f5015
Anemaygi <gianemayumisan@gmail.com>
2023-10-28 14:01:25 -0300

    refactor: poor refactor of quiz play

--- src/pages/quiz_play.tsx
+++ src/pages/quiz_play.tsx
@@ -4,32 +4,38 @@ import Navbar from '../components/navbar';
 import Button from '../components/button';
 import { FiX, FiCheck } from "react-icons/fi";
 
-const quiz = [
-  { 
-  obra: 'Pretuguês', 
-  artista: 'Rosana Paulino',
-  descricao: '“É engraçado como eles gozam a gente quando a gente diz que é Framengo. Chamam a gente de ignorante dizendo que a gente fala errado. E de repente ignoram que a presença desse r no lugar do l nada mais é que a marca linguística de um idioma africano, no qual o l inexiste. Afinal, quem que é o ignorante?”, explicou Lélia Gonzalez sobre o “Pretugûes”.',
-  prompt: '',
-  img: "https://revistamuseu.com.br/site/images/revista-museu/noticias/not_2022/not_2022_10/bandeira.jpg",
-  resposta: 'humano',         
+const quiz1 = [
+  {
+    obra: 'Pretuguês',
+    artista: 'Rosana Paulino',
+    descricao: '“É engraçado como eles gozam a gente quando a gente diz que é Framengo. Chamam a gente de ignorante dizendo que a gente fala errado. E de repente ignoram que a presença desse r no lugar do l nada mais é que a marca linguística de um idioma africano, no qual o l inexiste. Afinal, quem que é o ignorante?”, explicou Lélia Gonzalez sobre o “Pretugûes”.',
+    prompt: '',
+    img: "https://revistamuseu.com.br/site/images/revista-museu/noticias/not_2022/not_2022_10/bandeira.jpg",
+    resposta: 'humano',
   },
-  { 
-    obra: '/aaa', 
+  {
+    obra: '/aaa',
     artista: 'Homeaa',
     descricao: 'Homeaa',
     prompt: 'Homeaaa',
     img: "https://static.vecteezy.com/ti/fotos-gratis/t2/22653879-fantasia-ilha-com-cachoeiras-3d-ilustracao-elementos-do-isto-imagem-mobiliado-de-nasa-generativo-ai-gratis-foto.jpg",
-    resposta: 'ia',         
-    },
-  
+    resposta: 'ia',
+  },
+
 ];
+
+const quiz = [
+  quiz1[0],
+]
+
+quiz.push(quiz1[1]);
 
 const QuizPlay: React.FC = () => {
 
   const [idx, setIdx] = useState(0);
   const [points, setPoints] = useState(0);
   const [status, setStatus] = useState('na');  // na -> no answer // a1 -> already answer ia // a2 -> already answer humano
-  
+
 
   return (
     <div className="bg-[#0D0D0D] font-ZenMaru text-timberWolf h-screen  w-screen flex items-center p-2">
@@ -37,54 +43,56 @@ const QuizPlay: React.FC = () => {
         <Navbar />
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 m-2 h-full items-center">
-          
-            <div className={`space-y-2 text-2xl p-4 " + ${status == 'na' ? 'hidden' : 'opacity-1'} `}>
-                  <p className={ quiz[idx].resposta == 'ia' ? 'hidden' : ''  }><b>Obra:</b> {quiz[idx].obra}</p> 
-                  <p className={ quiz[idx].resposta == 'ia' ? 'hidden' : ''  }><b>Artista:</b> {quiz[idx].artista}</p>
-                  <p className={ quiz[idx].resposta == 'humano' ? 'hidden' : ''  }><b>Prompt:</b> {quiz[idx].prompt}</p>
-                  <p className="text-lg">{quiz[idx].descricao}</p>
-              </div>
-          
-          <div className={ status == 'na' ? `col-span-2` : `col-span-1`}>
-                <div className="h-full flex justify-center items-center ">
-                  <img className="object-scale-down" src={quiz[idx].img}/>
-                </div>
+
+          <div className={`space-y-2 text-2xl p-4 " + ${status == 'na' ? 'hidden' : 'opacity-1'} `}>
+            <p className={quiz[idx].resposta == 'ia' ? 'hidden' : ''}><b>Obra:</b> {quiz[idx].obra}</p>
+            <p className={quiz[idx].resposta == 'ia' ? 'hidden' : ''}><b>Artista:</b> {quiz[idx].artista}</p>
+            <p className={quiz[idx].resposta == 'humano' ? 'hidden' : ''}><b>Prompt:</b> {quiz[idx].prompt}</p>
+            <p className="text-lg">{quiz[idx].descricao}</p>
+          </div>
+
+          <div className={status == 'na' ? `col-span-2` : `col-span-1`}>
+            <div className="h-full flex justify-center items-center ">
+              <img className="object-scale-down" src={quiz[idx].img} />
+            </div>
           </div>
           <div className=" w-full h-fit flex flex-col justify-end">
-            <div className="border my-2 shadow-md rounded-full border-solid  border-grayTheme group" 
+            <div className="border my-2 shadow-md rounded-full border-solid  border-grayTheme group"
               onClick={() => {
-                if (status == 'na'){ 
-                  if (quiz[idx].resposta == 'ia') setPoints(points+1);
-                  setStatus('a1');}
-                }}>
-                <div 
-                className={`text-timberWolf border rounded-full border-solid  transition-all border-timberWolf p-1 m-1.5 flex ${status == 'na' ? 'group-hover:bg-timberWolf  cursor-pointer group-hover:text-night' : status == 'a1' && quiz[idx].resposta=='ia' ? 'bg-[#45877B]' : status == 'a1' && quiz[idx].resposta!='ia' ? 'bg-persianRed' : ''} `}>
-                  <p className="flex flex-grow px-3 py-0.5 text-lg">Gerado por inteligência artificial</p>
-                  <p className={`flex items-center mx-2 ${status == 'a1' ? '' : 'hidden'}`}> {quiz[idx].resposta=='ia' ? <FiCheck size={20}/> : <FiX size={20}/> } </p>
-                </div>
+                if (status == 'na') {
+                  if (quiz[idx].resposta == 'ia') setPoints(points + 1);
+                  setStatus('a1');
+                }
+              }}>
+              <div
+                className={`text-timberWolf border rounded-full border-solid  transition-all border-timberWolf p-1 m-1.5 flex ${status == 'na' ? 'group-hover:bg-timberWolf  cursor-pointer group-hover:text-night' : status == 'a1' && quiz[idx].resposta == 'ia' ? 'bg-[#45877B]' : status == 'a1' && quiz[idx].resposta != 'ia' ? 'bg-persianRed' : ''} `}>
+                <p className="flex flex-grow px-3 py-0.5 text-lg">Gerado por inteligência artificial</p>
+                <p className={`flex items-center mx-2 ${status == 'a1' ? '' : 'hidden'}`}> {quiz[idx].resposta == 'ia' ? <FiCheck size={20} /> : <FiX size={20} />} </p>
               </div>
+            </div>
 
-              <div className="border my-2 shadow-md rounded-full border-solid border-grayTheme group" 
-                onClick={() => {
-                  if (status == 'na'){ 
-                    if (quiz[idx].resposta == 'humano') setPoints(points+1);
-                  setStatus('a2');}
-                  }}>
-              <div 
-                className={`text-timberWolf border rounded-full border-solid  transition-all border-timberWolf p-1 m-1.5 flex ${status == 'na' ? 'group-hover:bg-timberWolf  cursor-pointer group-hover:text-night' : status == 'a2' && quiz[idx].resposta=='humano' ? 'bg-[#45877B]' : status == 'a2' && quiz[idx].resposta!='humano' ? 'bg-persianRed' : ''} `}>
-                  <p className="flex flex-grow px-3 py-0.5 text-lg">Criado por humanos</p>
-                  <p className={`flex items-center mx-2 ${status == 'a2' ? '' : 'hidden'}`}> {quiz[idx].resposta=='humano' ? <FiCheck size={20}/> : <FiX size={20}/> } </p>
-                </div>
-              </div></div>
+            <div className="border my-2 shadow-md rounded-full border-solid border-grayTheme group"
+              onClick={() => {
+                if (status == 'na') {
+                  if (quiz[idx].resposta == 'humano') setPoints(points + 1);
+                  setStatus('a2');
+                }
+              }}>
+              <div
+                className={`text-timberWolf border rounded-full border-solid  transition-all border-timberWolf p-1 m-1.5 flex ${status == 'na' ? 'group-hover:bg-timberWolf  cursor-pointer group-hover:text-night' : status == 'a2' && quiz[idx].resposta == 'humano' ? 'bg-[#45877B]' : status == 'a2' && quiz[idx].resposta != 'humano' ? 'bg-persianRed' : ''} `}>
+                <p className="flex flex-grow px-3 py-0.5 text-lg">Criado por humanos</p>
+                <p className={`flex items-center mx-2 ${status == 'a2' ? '' : 'hidden'}`}> {quiz[idx].resposta == 'humano' ? <FiCheck size={20} /> : <FiX size={20} />} </p>
+              </div>
+            </div></div>
 
 
           <div className={`w-full  h-fit flex justify-end items-end ${status == 'na' ? `hidden` : `opacity-1`}`}>
             <div className="m-3 w-56">
-                <Button handleClick={()=>{
-                  setIdx(idx+1);
-                  setStatus('na');
-                }} text="Continuar"/>
-              </div></div>
+              <Button handleClick={() => {
+                setIdx(idx + 1);
+                setStatus('na');
+              }} text="Continuar" />
+            </div></div>
         </div>
 
       </div>
