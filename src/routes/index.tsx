4405a40a9c9af439ca5f72e7f5a9518239f22f92
Anemaygi <gianemayumisan@gmail.com>
2023-10-28 13:31:49 -0300

    feat: add quiz functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "../pages/landingPage";
 import Quiz from "../pages/quiz_onboarding";
+import QuizPlay from "../pages/quiz_play";
 
 export default function RootRouter() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/quiz' element={<Quiz />} />
+      <Route path='/quizplay' element={<QuizPlay />} />s
     </Routes>
   );
 }
