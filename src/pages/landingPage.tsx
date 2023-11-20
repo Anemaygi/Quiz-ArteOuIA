41db2fade7e17213f38593d52f68fe3af5f49d36
Anemaygi <gianemayumisan@gmail.com>
2023-11-20 19:37:06 -0300

    add new text

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -15,7 +15,7 @@ const LandingPage: React.FC = () => {
           <div className="my-8">
 
             <h1 className='font-Oswald text-5xl font-bold mb-4 leading-snug text-center lg:text-left '> Como a tecnologia perpetua racismo e outros preconceitos? </h1>
-            <p className='font-ZenMaru text-2xl text-bold text-center lg:text-left'>Lorem ipsum dolor sit amet, consectetur dipiscing.</p>
+            <p className='font-ZenMaru text-2xl text-bold text-center lg:text-left'>A influência da tecnologia na perpetuação de viéses</p>
             <div className="flex justify-center items-center lg:justify-start mt-4">
               <div className="w-44"><Button text="Quiz" handleClick={() => navigate('/quiz')} /></div>
               <p className="cursor-pointer text-lg px-6 hover:scale-105" onClick={() => navigate('/about')}>Entenda</p>
--- src/pages/quiz_tips.tsx
+++ src/pages/quiz_tips.tsx
@@ -12,7 +12,7 @@ const Card: React.FC<CardProps> = ({ title, content }) => {
 
     <div className="bg-gradient-to-b rounded-lg shadow-md from-[#282828] h-full w-full flex lg:flex-row flex-col md:hover:bg-gradient-to-b md:hover:from-[#313131] transition-all ease-in-out md:hover:duration-300 items-center p-2 group">
       <h1 className="font-Oswald text-semibold text-3xl p-4 md:group-hover:text-white transition-all">{title}</h1>
-      <p className="font-ZenMaru text-lg text-center overflow-auto">{content}</p>
+      <p className="font-ZenMaru text-md text-justify overflow-auto">{content}</p>
     </div>
 
   );
@@ -34,17 +34,17 @@ const QuizTips: React.FC<QuizOnboardingProps> = ({ next }) => {
         </div>
       </div>
 
-      <div className="w-full h-full bg-gradient-to-b rounded-lg shadow-md from-[#282828] flex flex-col md:hover:bg-gradient-to-b items-center justify-center md:hover:from-[#313131] transition-all ease-in-out md:hover:duration-300 p-8 group">
+      <div className="w-full overflow-y-auto h-full bg-gradient-to-b rounded-lg shadow-md from-[#282828] flex flex-col md:hover:bg-gradient-to-b items-center justify-center md:hover:from-[#313131] transition-all ease-in-out md:hover:duration-300 p-8 group">
         <h1 className="font-Oswald text-semibold text-[2.5rem] p-4 md:group-hover:text-white transition-all">Como I.A. cria "arte"?</h1>
-        <img className="m-8" src="assets\iaarte.png" />
-        <p className="font-ZenMaru text-lg text-center overflow-auto">Lorem ipsum dolor sit amet, consectetur dipiscing.Lorem ipsum dolor sit amet, consectetur dipiscing.Lorem ipsum dolor sit amet, consectetur dipiscing.Lorem ipsum dolor sit amet, consectetur dipiscing.</p>
+        <img className="m-8 w-40" src="assets\iaarte.png" />
+        <p className="font-ZenMaru text-md text-center overflow-auto">Em 2014, surgiu um novo método de aprendizado de máquina chamado Generative Adversarial Network (GAN), adotado especialmente por artistas devido à capacidade de criar não apenas "arte visual", mas várias outras aplicações de geração de imagem por Inteligência Artificial. Para gerar imagens, esses modelos usam grandes conjuntos de dados, contendo milhares ou até milhões de exemplos de imagens reais, para treinar o Gerador (responsável por criar imagens) e o Discriminador (que avalia se a imagem gerada parece real). Ambos aprendem juntos para produzir resultados mais convincentes, capturando padrões e características estatísticas do conjunto de dados de treinamento.</p>
 
 
       </div>
       <div className="w-full grid grid-rows-3 h-full gap-4">
-        <Card title={'Subjetividade'} content={'Lorem ipsum dolor sit amet, consectetur dipiscing.'} />
-        <Card title={'Subjetividade'} content={'Lorem ipsum dolor sit amet, consectetur dipiscing.'} />
-        <Card title={'Subjetividade'} content={'Lorem ipsum dolor sit amet, consectetur dipiscing.'} />
+      <Card title={'Subjetividade'} content={'A I.A. pode ampliar a criatividade dos artistas, mas não tem a capacidade de expressar vivências com criatividade e subjetividade.'} />
+        <Card title={'Autoria'} content={'O modelo recebe instruções, mas utiliza dados já existentes (que podem ter direitos autorais) para criar resultados com características semelhantes.'} />
+        <Card title={'Viés'} content={'A utilização de dados pode levar à perpetuação de discriminações existentes na sociedade nos resultados.'} />
       </div>
 
     </div>
