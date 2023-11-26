ffeccb6319e1ba517be05623e3bf7235baa0e5f5
Anemaygi <gianemayumisan@gmail.com>
2023-11-25 21:16:13 -0300

    style: fix responsiveness

--- src/pages/quiz_tips.tsx
+++ src/pages/quiz_tips.tsx
@@ -34,7 +34,7 @@ const QuizTips: React.FC<QuizOnboardingProps> = ({ next }) => {
         </div>
       </div>
 
-      <div className="w-full overflow-y-auto h-full bg-gradient-to-b rounded-lg shadow-md from-[#282828] flex flex-col md:hover:bg-gradient-to-b items-center justify-center md:hover:from-[#313131] transition-all ease-in-out md:hover:duration-300 p-8 group">
+      <div className="w-full overflow-y-auto h-screen lg:h-full bg-gradient-to-b rounded-lg shadow-md from-[#282828] flex flex-col md:hover:bg-gradient-to-b items-center justify-center md:hover:from-[#313131] transition-all ease-in-out md:hover:duration-300 p-8 group">
         <h1 className="font-Oswald text-semibold text-[2.5rem] p-4 md:group-hover:text-white transition-all">Como I.A. cria "arte"?</h1>
         <img className="m-8 w-40" src="assets\iaarte.png" />
         <p className="font-ZenMaru text-md text-center overflow-auto">Em 2014, surgiu um novo método de aprendizado de máquina chamado Generative Adversarial Network (GAN), adotado especialmente por artistas devido à capacidade de criar não apenas "arte visual", mas várias outras aplicações de geração de imagem por Inteligência Artificial. Para gerar imagens, esses modelos usam grandes conjuntos de dados, contendo milhares ou até milhões de exemplos de imagens reais, para treinar o Gerador (responsável por criar imagens) e o Discriminador (que avalia se a imagem gerada parece real). Ambos aprendem juntos para produzir resultados mais convincentes, capturando padrões e características estatísticas do conjunto de dados de treinamento.</p>
